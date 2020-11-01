486a4c4b454884ea17b71177dac8e10053d947f6
Nathan Taylor <nathan@nathantaylor.com>
2020-11-01 11:41:01 -0800

    Fixed issue in auth refresh logic

--- session.py
+++ session.py
@@ -18,28 +18,33 @@ class BlinkSession:
         return os.path.exists(self.auth_file)
 
     def _refresh_auth(self, reset=False):
+        self.blink.auth = Auth()
+
         if not reset and self._auth_exists():
             self.blink.auth = Auth(json_load(self.auth_file))
+        else:
+            self.blink.auth = Auth()
 
         self.blink.start()
-        # update auth file
+
+        # write auth file
         self.blink.save(self.auth_file)
+        print('Auth file updated: ' + self.auth_file)
 
-        return self.blink
+        return self.get(force_reset=False)
 
-    def get(self, force_refresh=False):
-        if force_refresh or not self._auth_exists():
-            self.blink = self._refresh_auth(True)
+    def get(self, force_reset=False):
+        if force_reset or not self._auth_exists():
+            self.blink = self._refresh_auth(reset=True)
         else:
             self.blink.auth = Auth(login_data=json_load(self.auth_file), no_prompt=True)
             self.blink.start()
 
             if len(self.blink.cameras) == 0:
-                self.blink = self._refresh_auth()
+                return self._refresh_auth(reset=False)
 
         return self.blink
 
 
 if __name__ == '__main__':
-    BlinkSession().get(force_refresh=True)
-    print('Credentials updated')
+    BlinkSession().get(force_reset=True)
