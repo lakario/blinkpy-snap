e37d1da7b2f948299caafbb45cea62dda57ad1d4
Nathan Taylor <nathan@nathantaylor.com>
2021-01-15 08:28:24 -0800

    - Fixed issue in auth refresh logic - Added retry when thumbnail update check fails

--- run.py
+++ run.py
@@ -15,6 +15,15 @@ def get_camera_image(camera_name, image_path='images'):
 
     blink_session.refresh(force=True)
 
+    retry_count = 0
+    max_retries = 3
+
+    while before_thumb == camera.attributes['thumbnail'] and retry_count < max_retries:
+        print('ERROR getting updated thumbnail. Retrying.')
+        time.sleep(2)
+        blink_session.refresh(force=True)
+        retry_count += 1
+
     if before_thumb == camera.attributes['thumbnail']:
         print('ERROR: Thumbnail was not updated. Aborting.')
         exit(1)
@@ -28,5 +37,15 @@ def get_camera_image(camera_name, image_path='images'):
         print('Image saved to: ' + image_name)
 
 
+def setup_logger():
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(levelname)-8s [%(asctime)s] [%(name)s] %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename='run.log')
+
+
 if __name__ == '__main__':
-    get_camera_image('Farm', 'images')
+    setup_logger()
+    get_camera_image('Tent', 'images/tent')
--- session.py
+++ session.py
@@ -2,6 +2,7 @@ import os.path
 from blinkpy.auth import Auth
 from blinkpy.blinkpy import Blink
 from blinkpy.helpers.util import json_load
+import time
 
 AUTH_FILE = "credentials.json"
 
@@ -14,14 +15,16 @@ class BlinkSession:
         self.auth_file = AUTH_FILE
         self.blink = Blink()
 
-    def _auth_exists(self):
+    def _auth_file_exists(self):
         return os.path.exists(self.auth_file)
 
     def _refresh_auth(self, reset=False):
-        self.blink.auth = Auth()
+        self.blink = Blink()
+        with_sleep = False
 
-        if not reset and self._auth_exists():
+        if not reset and self._auth_file_exists():
             self.blink.auth = Auth(json_load(self.auth_file))
+            with_sleep = True
         else:
             self.blink.auth = Auth()
 
@@ -31,16 +34,19 @@ class BlinkSession:
         self.blink.save(self.auth_file)
         print('Auth file updated: ' + self.auth_file)
 
+        if with_sleep:
+            time.sleep(3)
+
         return self.get(force_reset=False)
 
     def get(self, force_reset=False):
-        if force_reset or not self._auth_exists():
+        if force_reset or not self._auth_file_exists():
             self.blink = self._refresh_auth(reset=True)
         else:
             self.blink.auth = Auth(login_data=json_load(self.auth_file), no_prompt=True)
             self.blink.start()
 
-            if len(self.blink.cameras) == 0:
+            if not self.blink.available:
                 return self._refresh_auth(reset=False)
 
         return self.blink
